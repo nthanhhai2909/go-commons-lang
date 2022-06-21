7cdc0b6f131fd6525b3266aa48b9c954229112db
hai.t.nguyen <hai.t.nguyen@opswat.com>
2022-06-21 12:05:30 +0700

    add common funcs to stringutils

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -1,6 +1,13 @@
 package stringutils
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
+
+const (
+	EMPTY = ""
+)
 
 /**
  * <p>Checks if a string is empty ("").</p>
@@ -80,4 +87,252 @@ func IsBlank(cs string) bool {
 
 func IsNotBlank(cs string) bool {
 	return !IsBlank(cs)
+}
+
+/**
+ * <p>Checks if the string contains only Unicode digits.
+ * A decimal point is not a Unicode digit and returns false.</p>
+ * An empty string (length()=0) will return {@code false}.</p>
+ *
+ * <pre>
+ * IsNumeric("")     = false
+ * IsNumeric("  ")   = false
+ * IsNumeric("123")  = true
+ * IsNumeric("\u0967\u0968\u0969")  = true
+ * IsNumeric("12 3") = false
+ * IsNumeric("ab2c") = false
+ * IsNumeric("12-3") = false
+ * IsNumeric("12.3") = false
+ * IsNumeric("-123") = false
+ * isNumeric("+123") = false
+ * </pre>
+ *
+ * @param cs  the string to check
+ * @return {@code true} if only contains digits
+ */
+
+func IsNumeric(cs string) bool {
+	if IsEmpty(cs) {
+		return false
+	}
+
+	for _, s := range cs {
+		if !unicode.IsDigit(s) {
+			return false
+		}
+	}
+	return true
+}
+
+/**
+ * <p>Checks if the string contains only Unicode digits or whitespace.
+ * A decimal point is not a Unicode digit and returns false.</p>
+ * An empty string (length()=0) will return {@code false}.</p>
+ *
+ * <pre>
+ * IsNumericSpace("")     = true
+ * IsNumericSpace("  ")   = true
+ * IsNumericSpace("123")  = true
+ * IsNumericSpace("\u0967\u0968\u0969")  = true
+ * IsNumericSpace("12 3") = true
+ * IsNumericSpace("ab2c") = false
+ * IsNumericSpace("12-3") = false
+ * IsNumericSpace("12.3") = false
+ * IsNumericSpace("-123") = false
+ * IsNumericSpace("+123") = false
+ * </pre>
+ *
+ * @param cs  the string to check
+ * @return {@code true} if only contains digits
+ */
+
+func IsNumericSpace(cs string) bool {
+	for _, s := range cs {
+		if !unicode.IsSpace(s) && !unicode.IsDigit(s) {
+			return false
+		}
+	}
+	return true
+}
+
+/**
+ * <p>Checks if the string contains only whitespace.</p>
+ *
+ * An empty string (length()=0) will return {@code true}.</p>
+ *
+ * IsWhiteSpace("")     = true
+ * IsWhiteSpace("  ")   = true
+ * IsWhiteSpace("abc")  = false
+ * IsWhiteSpace("ab2c") = false
+ * IsWhiteSpace("ab-c") = false
+ * </pre>
+ *
+ * @param cs  the CharSequence to check
+ * @return {@code true} if only contains whitespace
+ */
+
+func IsWhiteSpace(cs string) bool {
+	for _, s := range cs {
+		if !unicode.IsSpace(s) {
+			return false
+		}
+	}
+
+	return true
+}
+
+/**
+ * <p>Converts a String to upper case as per {@link strings#ToUpper()}.</p>
+ * <pre>
+ * UpperCase("")    = ""
+ * UpperCase("aBc") = "ABC"
+ * </pre>
+ * @param str  the String to upper case
+ * @return the upper case String
+ */
+
+func UpperCase(cs string) string {
+	return strings.ToUpper(cs)
+}
+
+/**
+ * <p>Converts a String to lowser case as per {@link strings#ToLower()}.</p>
+ * <pre>
+ * UpperCase("")    = ""
+ * UpperCase("aBc") = "abc"
+ * </pre>
+ * @param str  the String to upper case
+ * @return the upper case String
+ */
+
+func LowerCase(cs string) string {
+	return strings.ToLower(cs)
+}
+
+/**
+ * <p>Checks if the string contains only uppercase characters.</p>
+ *
+ * An empty String (length()=0) will return {@code false}.</p>
+ *
+ * <pre>
+ * IsAllUpperCase("")     = false
+ * IsAllUpperCase("  ")   = false
+ * IsAllUpperCase("ABC")  = true
+ * IsAllUpperCase("aBC")  = false
+ * IsAllUpperCase("A C")  = false
+ * IsAllUpperCase("A1C")  = false
+ * IsAllUpperCase("A/C")  = false
+ * </pre>
+ *
+ * @param cs the string to check
+ * @return {@code true} if only contains uppercase characters
+ */
+
+func IsAllUpperCase(cs string) bool {
+	if IsEmpty(cs) {
+		return false
+	}
+	for _, s := range cs {
+		if !unicode.IsUpper(s) {
+			return false
+		}
+	}
+	return true
+}
+
+/**
+ * <p>Checks if the string contains only lowercase characters.</p>
+ *
+ * An empty string (length()=0) will return {@code false}.</p>
+ *
+ * <pre>
+ * IsAllLowerCase("")     = false
+ * IsAllLowerCase("  ")   = false
+ * IsAllLowerCase("abc")  = true
+ * IsAllLowerCase("abC")  = false
+ * IsAllLowerCase("ab c") = false
+ * IsAllLowerCase("ab1c") = false
+ * IsAllLowerCase("ab/c") = false
+ * </pre>
+ *
+ * @param cs  the CharSequence to check
+ * @return {@code true} if only contains lowercase characters
+ */
+
+func IsAllLowerCase(cs string) bool {
+	if IsEmpty(cs) {
+		return false
+	}
+
+	for _, s := range cs {
+		if !unicode.IsLower(s) {
+			return false
+		}
+	}
+
+	return true
+}
+
+/**
+ * <p>Checks if the string contains only Unicode letters.</p>
+ *
+ * An empty CharSequence (length()=0) will return {@code false}.</p>
+ *
+ * <pre>
+ * IsAlpha("")     = false
+ * IsAlpha("  ")   = false
+ * IsAlpha("abc")  = true
+ * IsAlpha("ab2c") = false
+ * IsAlpha("ab-c") = false
+ * </pre>
+ *
+ * @param cs  the string to check
+ * @return {@code true} if only contains letters
+ */
+
+func IsAlpha(cs string) bool {
+	if IsEmpty(cs) {
+		return false
+	}
+
+	for _, s := range cs {
+		if !unicode.IsLetter(s) {
+			return false
+		}
+	}
+
+	return true
+}
+
+/**
+ * <p>Checks if the string contains only Unicode letters or digits.</p>
+ *
+ * An empty CharSequence (length()=0) will return {@code false}.</p>
+ *
+ * <pre>
+
+ * IsAlphanumeric("")     = false
+ * IsAlphanumeric("  ")   = false
+ * IsAlphanumeric("abc")  = true
+ * IsAlphanumeric("ab c") = false
+ * IsAlphanumeric("ab2c") = true
+ * IsAlphanumeric("ab-c") = false
+ * </pre>
+ *
+ * @param cs  the CharSequence to check
+ * @return {@code true} if only contains letters or digits,
+ */
+
+func IsAlphanumeric(cs string) bool {
+	if IsEmpty(cs) {
+		return false
+	}
+
+	for _, s := range cs {
+		if !unicode.IsLetter(s) && !unicode.IsDigit(s) {
+			return false
+		}
+	}
+
+	return true
 }
--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -3,7 +3,7 @@ package stringutils
 import "testing"
 
 func TestIsEmpty(t *testing.T) {
-	if !IsEmpty("") ||
+	if !IsEmpty(EMPTY) ||
 		IsEmpty(" ") ||
 		IsEmpty("bob") ||
 		IsEmpty("  bob  ") {
@@ -12,7 +12,7 @@ func TestIsEmpty(t *testing.T) {
 }
 
 func TestIsNotEmpty(t *testing.T) {
-	if IsNotEmpty("") ||
+	if IsNotEmpty(EMPTY) ||
 		!IsNotEmpty(" ") ||
 		!IsNotEmpty("bob") ||
 		!IsNotEmpty("  bob  ") {
@@ -21,7 +21,7 @@ func TestIsNotEmpty(t *testing.T) {
 }
 
 func TestIsBlank(t *testing.T) {
-	if !IsBlank("") ||
+	if !IsBlank(EMPTY) ||
 		!IsBlank(" ") ||
 		IsBlank("bob") ||
 		IsBlank("  bob  ") {
@@ -30,10 +30,112 @@ func TestIsBlank(t *testing.T) {
 }
 
 func TestIsNotBlank(t *testing.T) {
-	if IsNotBlank("") ||
+	if IsNotBlank(EMPTY) ||
 		IsNotBlank(" ") ||
 		!IsNotBlank("bob") ||
 		!IsNotBlank("  bob  ") {
 		t.Errorf("IsNotBlank func don't work correctly")
+	}
+}
+
+func TestIsNumeric(t *testing.T) {
+	if IsNumeric(EMPTY) ||
+		IsNumeric("  ") ||
+		IsNumeric("12 3") ||
+		IsNumeric("ab2c") ||
+		IsNumeric("12-3") ||
+		IsNumeric("12.3") ||
+		IsNumeric("-123") ||
+		IsNumeric("+123") ||
+		!IsNumeric("123") ||
+		!IsNumeric("\u0967\u0968\u0969") {
+		t.Errorf("IsNumeric func don't work correctly")
+	}
+}
+
+func TestIsNumericSpace(t *testing.T) {
+	if !IsNumericSpace("  ") ||
+		!IsNumericSpace("123") ||
+		!IsNumericSpace("\u0967\u0968\u0969") ||
+		!IsNumericSpace("12 3") ||
+		!IsNumericSpace("") ||
+		IsNumericSpace("ab2c") ||
+		IsNumericSpace("12-3") ||
+		IsNumericSpace("12.3") ||
+		IsNumericSpace("-123") ||
+		IsNumericSpace("+123") {
+		t.Errorf("IsNumericSpace func don't work correctly")
+	}
+}
+
+func TestIsWhiteSpace(t *testing.T) {
+	if !IsWhiteSpace(EMPTY) ||
+		!IsWhiteSpace("  ") ||
+		IsWhiteSpace("abc") ||
+		IsWhiteSpace("ab2c") ||
+		IsWhiteSpace("ab-c") {
+		t.Errorf("IsWhiteSpace func don't work correctly")
+	}
+}
+
+func TestUpperCase(t *testing.T) {
+	if UpperCase(EMPTY) != "" ||
+		UpperCase("aBc") != "ABC" ||
+		UpperCase("abc") != "ABC" {
+		t.Errorf("UpperCase func don't work correctly")
+	}
+}
+
+func TestLowerCase(t *testing.T) {
+	if LowerCase(EMPTY) != "" ||
+		LowerCase("aBc") != "abc" ||
+		LowerCase("abc") != "abc" ||
+		LowerCase("ABC") != "abc" {
+		t.Errorf("LowerCase func don't work correctly")
+	}
+}
+
+func TestIsAllUpperCase(t *testing.T) {
+	if IsAllUpperCase(EMPTY) ||
+		IsAllUpperCase("  ") ||
+		IsAllUpperCase("aBC") ||
+		IsAllUpperCase("A C") ||
+		IsAllUpperCase("A1C") ||
+		IsAllUpperCase("A/C") ||
+		!IsAllUpperCase("ABC") {
+		t.Errorf("IsAllUpperCase func don't work correctly")
+	}
+}
+
+func TestIsAllLowerCase(t *testing.T) {
+	if IsAllLowerCase(EMPTY) ||
+		IsAllLowerCase("  ") ||
+		IsAllLowerCase("abC") ||
+		IsAllLowerCase("ab c") ||
+		IsAllLowerCase("ab1c") ||
+		IsAllLowerCase("ab/c") ||
+		!IsAllLowerCase("abc") {
+		t.Errorf("IsAllLowerCase func don't work correctly")
+	}
+}
+
+func TestIsAlpha(t *testing.T) {
+	if IsAlpha(EMPTY) ||
+		IsAlpha("  ") ||
+		!IsAlpha("abc") ||
+		IsAlpha("ab2c") ||
+		IsAlpha("ab-c") {
+		t.Errorf("IsAlpha func don't work correctly")
+	}
+}
+
+func TestIsAlphanumeric(t *testing.T) {
+	if IsAlphanumeric(EMPTY) ||
+		IsAlphanumeric("  ") ||
+		!IsAlphanumeric("abc") ||
+		IsAlphanumeric("ab c") ||
+		!IsAlphanumeric("ab2c") ||
+		IsAlphanumeric("ab-c") {
+		t.Errorf("IsAlphanumeric func don't work correctly")
 	}
 }
