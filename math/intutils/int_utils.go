817d310e69357172597f50c2b1828cb45c26c43f
hai.t.nguyen <hai.t.nguyen@opswat.com>
2022-06-24 11:10:21 +0700

    update generic for ini utils

--- math/intutils/int_utils.go
+++ math/intutils/int_utils.go
@@ -1,28 +1,33 @@
 package intutils
 
-func Abs(num int) int {
+type IntNumber interface {
+	int | int8 | int16 | int32 | int64
+}
+
+func Abs[T IntNumber](num T) T {
 	if num > 0 {
 		return num
 	}
 	return -num
 }
 
-func Sum(nums ...int) int {
-	sum := 0
+func Sum[T IntNumber](nums ...T) T {
+	var sum T
+	sum = 0
 	for _, num := range nums {
 		sum += num
 	}
 	return sum
 }
 
-func Max(a, b int) int {
+func Max[T IntNumber](a, b T) T {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func Min(a, b int) int {
+func Min[T IntNumber](a, b T) T {
 	if a < b {
 		return a
 	}
